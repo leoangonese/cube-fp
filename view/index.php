273a910d2adb29053a89b3946347dd87185d8e92
Leonardo Staniecki Angonese <koala.leo17@gmail.com>
2022-07-19 23:24:48 -0300

    adjuted the error in the login

--- view/index.php
+++ view/index.php
@@ -10,35 +10,6 @@
 
 <body>
 
-	<?php
-
-		if(!isset($_SESSION['login'])){
-
-			if(isset($_POST['acao'])){
-				$login = 'joao';
-				$senha = 'senha';
-
-				$loginForm = $_POST['login'];
-				$senhaForm = $_POST['senha'];
-
-				if($login == $loginForm && $senha == $senhaForm){
-					$_SESSION['login'] = true;
-					header('Location: index.php');
-				}else{
-					echo 'Dados inválidos.';
-				}
-			}
-
-			include('login.php');
-		}else{
-			if(isset($_GET['logout'])){
-				unset($_SESSION['login']);
-				session_destroy();
-				header('Location: index.php');
-			}
-			include('cadProprietario.php');
-		}
-
 	<div class="wrapper">
 	<nav id="sidebar" class="sidebar js-sidebar">
 		<?php include 
