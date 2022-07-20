303c9399ac27154a0b2795dd622e589bcf346336
Joao Silveira <joaovictormachadosilveira@hotmail.com>
2022-07-19 22:15:39 -0300

    Created login w/ errors

--- view/index.php
+++ view/index.php
@@ -1,9 +1,44 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+	<title>Login</title>
+</head>	
 
 <?php include "head.php"; ?>
 
 <body>
+
+	<?php
+
+		if(!isset($_SESSION['login'])){
+
+			if(isset($_POST['acao'])){
+				$login = 'joao';
+				$senha = 'senha';
+
+				$loginForm = $_POST['login'];
+				$senhaForm = $_POST['senha'];
+
+				if($login == $loginForm && $senha == $senhaForm){
+					$_SESSION['login'] = true;
+					header('Location: index.php');
+				}else{
+					echo 'Dados inválidos.';
+				}
+			}
+
+			include('login.php');
+		}else{
+			if(isset($_GET['logout'])){
+				unset($_SESSION['login']);
+				session_destroy();
+				header('Location: index.php');
+			}
+			include('cadProprietario.php');
+		}
+
 	<div class="wrapper">
 	<nav id="sidebar" class="sidebar js-sidebar">
 		<?php include 
