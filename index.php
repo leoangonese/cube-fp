fa6445ff8f48a2225b6b3f0081bb697c04b27d12
Leonardo Staniecki Angonese <koala.leo17@gmail.com>
2022-07-21 18:56:51 -0300

    adjuted layout for login page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if(!isset($_SESSION['login'])){
 
    if(isset($_POST['acao'])){
-      $login = 'name';
+      $login = 'username';
       $senha = 'password';
 
       $loginForm = $_POST['login'];
@@ -12,11 +12,11 @@ if(!isset($_SESSION['login'])){
          $_SESSION['login'] = true;
          header('Location: ./view/index.php');
       }else{
-         echo 'Dados inválidos.';
+         echo '<h4 style="color: red; text-align:center; padding-top:50px">Dados inválidos.</h4>';
       }
    }
 
-   include('login.php');
+   include('./view/login.php');
 }else{
    if(isset($_GET['logout'])){
       unset($_SESSION['login']);
---
+++ view/login.php
@@ -0,0 +1,36 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<head>
+<title>Cube - Login</title>
+</head>
+
+
+<body class='bg-dark'>
+    <div>
+        <h3 class="text-center text-success pt-5">Cube Login</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form " action="" method="post">
+
+                            <div class="form-group">
+                                <label for="username" class="text-success">Username:</label><br>
+                                <input type="text" name="login" id="name" class="form-control text-dark">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-success">Password:</label><br>
+                                <input type="text" name="senha" id="password" class="form-control text-dark">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="acao" class="btn btn-outline-success btn-md" value="submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
